013d98c236dc31e1daf7e10fb8fa52198e6a8c2b
pgs <pgs@tid.es>
2016-04-22 11:44:51 +0200

    add java skeleton

---
+++ src/test/java/com/pguardiola/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.pguardiola;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testThatWorks() {
+        assertTrue(true);
+    }
+}
